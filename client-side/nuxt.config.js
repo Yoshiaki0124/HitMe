693e36d3ec2376b20e615608b089eca73d49eca4
“Yoshiaki0124” <yoshiakirecruit19930124@gmail.com>
2021-03-28 09:18:03 +0900

    first_layout

--- client-side/nuxt.config.js
+++ client-side/nuxt.config.js
@@ -1,17 +1,17 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  mode:"spa",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'front',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: 'HitMe',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { 
+        hid: 'description',
+        name: 'description', 
+        content: '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
